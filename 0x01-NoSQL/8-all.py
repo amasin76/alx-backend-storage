e4f1aca947b1c45c1b6d61c5d2224d60a9d79103
amasin76 <amasin76@gmail.com>
2024-01-23 00:29:42 +0000

    List all docs in py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """Return an empty list if no document in the collection"""
+    documents = mongo_collection.find()
+    return documents
